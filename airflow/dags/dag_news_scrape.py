a98023109176cb32ea0fc8f6ff20fc59387c433c
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2024-09-15 19:35:53 +0200

    Updated dag and finished pipeline

--- airflow/dags/dag_news_scrape.py
+++ airflow/dags/dag_news_scrape.py
@@ -4,8 +4,13 @@ from airflow.decorators import dag, task
 
 from bs4 import BeautifulSoup
 import requests
-from transformers import pipeline
 import csv
+import os
+
+# Set the TRANSFORMERS_CACHE environment variable
+os.environ['TRANSFORMERS_CACHE'] = '.'
+
+from transformers import pipeline
 
 default_args = {
     'owner': 'matea',
@@ -14,10 +19,10 @@ default_args = {
 }
 
 @dag(
-    dag_id='dag_diplomski_v2',
-    description='second dag for news classification with taskflow',
+    dag_id='dag_diplomski',
+    description='dag for news classification with taskflow',
     default_args=default_args,
-    start_date=datetime(2024,8,21,4),
+    start_date=datetime(2024,9,15,4),
     schedule_interval='@daily')
 def news_scraper_etl():
 
@@ -54,7 +59,7 @@ def news_scraper_etl():
     @task()
     def classification(to_classify: dict) -> dict:
         res = dict()
-        for pred, text in zip(pipeline("sentiment-analysis", model="Lukiccc/my_awesome_model")(list(to_classify.values())), list(to_classify.values())):
+        for pred, text in zip(pipeline("text-classification", model="Lukiccc/my_awesome_model")(list(to_classify.values())), list(to_classify.values())):
             res[text] = pred
         return res
     
@@ -83,43 +88,45 @@ def news_scraper_etl():
                 res += f"('{today}', '{i}', '{text}', '{label}')"
             else:
                 res += f",('{today}', '{i}', '{text}', '{label}')"
-        
-        print(res)
+    
         return res
     
-    create_database_task = PostgresOperator(
-        task_id='create_postgres_table',
-        postgres_conn_id='postgres_localhost',
-        sql='''
-            create table if not exists news(
-                dt date,
-                news_id character varying,
-                primary key (dt, news_id),
-                text character varying,
-                label character varying
-            )
-            '''
+    @task()
+    def insert_data(insert_string: dict, db_created: bool) -> str:
+        if not db_created:
+            raise ValueError("db is not created")
+        PostgresOperator(
+            task_id='insert_into_table',
+            postgres_conn_id='postgres_localhost',
+            sql=f'''
+                insert into news(dt, news_id, text, label) values {insert_string};
+                '''
         )
-    
+
+    @task
+    def create_database_task():
+        PostgresOperator(
+            task_id='create_postgres_table',
+            postgres_conn_id='postgres_localhost',
+            sql='''
+                create table if not exists news(
+                    dt date,
+                    news_id character varying,
+                    primary key (dt, news_id),
+                    text character varying,
+                    label character varying
+                )
+                '''
+            )
+        return True
+
+    db_created = create_database_task()    
     scraped_text = scrape()
     soup = get_soup(scraped_text)
     clean_text = clean_scraped(soup)
     res_dict = make_dict(clean_text)
     classified = classification(res_dict)
-    
-
-    insert_string = create_insert_string(classified)
-    insert_data = PostgresOperator(
-        task_id='insert_into_table',
-        postgres_conn_id='postgres_localhost',
-        sql=f'''
-            insert into news(dt, news_id, text, label) values {insert_string};
-            ''',
-        parameters ={"insert_string": insert_string}
-    )
-
-
-    create_database_task >> insert_data
+    insert_data(create_insert_string(classified), db_created)
     save_to_csv(classified)
 
 news_scraper_etl()
